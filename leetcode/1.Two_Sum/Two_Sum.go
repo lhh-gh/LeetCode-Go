2f07a65e6cd8b3eec3424d9ef115fdbc9e2caa7b
lhh-gh <sillycats@sina.com>
2024-11-24 23:19:34 +0800

    力扣第一次提交

---
+++ leetcode/1.Two_Sum/Two_Sum.go
@@ -0,0 +1,14 @@
+package leetcode
+
+func twoSum(nums []int, target int) []int {
+	//创建一个map m(哈希表) 来存储已经遍历过的数字及其索引
+	m := make(map[int]int)
+	for i, v := range nums {
+		//如果当前元素 v 没有找到配对，则将其及其索引 i 存入哈希表 m 中
+		if j, ok := m[target-v]; ok {
+			return []int{j, i}
+		}
+		m[v] = i
+	}
+	return nil
+}
